c90642aa57cbbdb918ebeaa75430ea3fbdbcfb42
shin <shinjiyong0904@gmail.com>
2025-10-15 21:41:14 +0900

    7주차 숙제

---
+++ Ch07-homework/contactRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const users = [
+    {id:1, name:"Kim"},
+    {id:2, name:"Lee"},
+    {id:3, name:"Park"},
+];
+
+router.get("/", (req,res) => {
+    res.status(200).json(users);
+});
+
+module.exports = router;    
+// 이 파일을 require로 가져올 때 router.get(...)객체를 반환하도록 설정
