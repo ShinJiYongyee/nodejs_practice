791ac2eff5d081ece4d3ac79552787d1adc4d962
shin <shinjiyong0904@gmail.com>
2025-11-18 14:57:51 +0900

    12주차 실습파일 준비

---
+++ Ch11src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const methodOverrid = require("method-override");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+dbConnect();
+
+//정적 파일 폴더(public) 등록
+app.use(express.static("./public"));
+
+app.use(methodOverrid("_method"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/contacts", require("./routes/contactRoutes"));
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 서버 실행 중`);
+});
